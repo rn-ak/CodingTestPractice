ee368c8279ba8bbab1613ea86f15b5ec2e56ea86
rn-ak <63904518+rn-ak@users.noreply.github.com>
2024-01-02 21:03:31 +0900

    Add files via upload

---
+++ 프로그래머스 MySQL1/level 2/루시와 엘라 찾기.sql
@@ -0,0 +1,8 @@
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS 
+where NAME like 'Lucy' 
+or NAME like 'Ella' 
+or NAME like 'Pickle' 
+or NAME like 'Rogan'
+or NAME like 'Sabrina'
+or NAME like 'Mitty'
---
+++ 프로그래머스 MySQL1/level 2/이름에 el이 들어가는 동물 찾기.sql
@@ -0,0 +1,4 @@
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS 
+where NAME like '%EL%' and ANIMAL_TYPE like 'Dog'
+order by NAME
